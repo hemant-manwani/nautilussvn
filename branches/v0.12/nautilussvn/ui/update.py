046c082a63ea63e7bd1ee35db14e4fd504242521
adamplumb <adamplumb@c1aae091-0650-0410-8f76-b90164cd595d>
2008-12-02 14:34:00 +0000

    added glade ui and python handler for update-to-revision

--- branches/v0.12/nautilussvn/ui/update.py
+++ branches/v0.12/nautilussvn/ui/update.py
@@ -4,6 +4,9 @@ import pygtk
 import gobject
 import gtk
 
+import component.widget
+
+import log
 import notification
 
 class Update:
@@ -16,7 +19,63 @@ class Update:
 
     def __init__(self):
         self.notification = notification.Notification()
+
+
+class UpdateToRevision:
+    """
+    This class provides an interface to update a working copy to a specific
+    revision.  It has a glade view.
+    
+    """
+    
+    DEPTHS = {
+        'a': 'Working Copy',
+        'b': 'Fully recursive',
+        'c': 'Immediate children, including folders',
+        'd': 'Only file children',
+        'e': 'Only this item'
+    }
+    
+    def __init__(self):
+        self.view = component.view.InterfaceView(self, "Update")
         
+        self.depth = component.widget.ComboBox(
+            self.view.get_widget("update_depth")
+        )
+        for i in self.DEPTHS.values():
+            self.depth.append(i)
+        self.depth.set_active(0)
+
+    def on_update_destroy(self, widget):
+        gtk.main_quit()
+
+    def on_update_cancel_clicked(self, widget):
+        gtk.main_quit()
+
+    def on_update_ok_clicked(self, widget):
+        self.view.hide()
+        self.notification = notification.Notification()
+
+    def on_update_revision_number_focused(self, widget, data=None):
+        self.view.get_widget("update_revision_number_opt").set_active(True)
+
+    def on_update_show_log_clicked(self, widget, data=None):
+        self.logview = LogForUpdate()
+
+class LogForUpdate(log.Log):
+    def __init__(self):
+        log.Log.__init__(self)
+        
+    def on_log_destroy(self, widget):
+        self.view.hide()
+    
+    def on_log_cancel_clicked(self, widget, data=None):
+        self.view.hide()
+    
+    def on_log_ok_clicked(self, widget, data=None):
+        self.view.hide()
+
+
 if __name__ == "__main__":
-    window = Update()
+    window = UpdateToRevision()
     gtk.main()
