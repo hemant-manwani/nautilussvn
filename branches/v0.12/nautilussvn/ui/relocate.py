fded5c4f1476a0b6cad48b576784f9a17b83ba47
adamplumb <adamplumb@c1aae091-0650-0410-8f76-b90164cd595d>
2008-12-03 02:22:16 +0000

    added ui and python script for relocate interface

---
+++ branches/v0.12/nautilussvn/ui/relocate.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import pygtk
+import gobject
+import gtk
+
+import component.view
+import component.helper
+
+import notification
+
+class Relocate:
+    """
+    Interface to relocate your working copy's repository
+    
+    """
+
+    def __init__(self):
+        self.view = component.view.InterfaceView(self, "Relocate")
+
+        self.repositories = component.widget.ComboBox(
+            self.view.get_widget("relocate_to_urls"), 
+            component.helper.GetRepositoryPaths()
+        )
+
+    def on_relocate_destroy(self, widget):
+        gtk.main_quit()
+
+    def on_relocate_cancel_clicked(self, widget):
+        gtk.main_quit()
+
+    def on_relocate_ok_clicked(self, widget):
+        self.view.hide()
+        self.notification = notification.Notification()
+
+if __name__ == "__main__":
+    window = Relocate()
+    gtk.main()
