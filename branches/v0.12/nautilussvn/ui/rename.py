203ea9f5f0a5a16b0c24b1d2b37cac3593bca231
adamplumb <adamplumb@c1aae091-0650-0410-8f76-b90164cd595d>
2008-12-05 14:42:40 +0000

    added rename ui

---
+++ branches/v0.12/nautilussvn/ui/rename.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pygtk
+import gobject
+import gtk
+
+import nautilussvn.ui
+import nautilussvn.ui.notification
+
+class Rename:
+    def __init__(self, filename=""):
+        self.view = nautilussvn.ui.InterfaceView(self, "rename", "Rename")
+        self.view.get_widget("new_name").set_text(filename)
+        
+    def on_destroy(self, widget):
+        gtk.main_quit()
+
+    def on_cancel_clicked(self, widget):
+        gtk.main_quit()
+
+    def on_ok_clicked(self, widget):
+        self.view.hide()
+        self.notification = nautilussvn.ui.notification.Notification()
+        
+if __name__ == "__main__":
+    window = Rename("test.txt")
+    gtk.main()
