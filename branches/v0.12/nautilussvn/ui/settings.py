4d64ee4c4215b540fccb0c20fabd4bc92b638ac7
adamplumb <adamplumb@c1aae091-0650-0410-8f76-b90164cd595d>
2008-12-11 15:05:12 +0000

    more trimming of the default settings, initial working ui with real settings

--- branches/v0.12/nautilussvn/ui/settings.py
+++ branches/v0.12/nautilussvn/ui/settings.py
@@ -12,7 +12,6 @@ class Settings:
 
         self.settings = nautilussvn.lib.settings.SettingsManager()
         
-
         self.language = nautilussvn.ui.widget.ComboBox(
             self.view.get_widget("language"), 
             ['English']
@@ -20,7 +19,31 @@ class Settings:
         self.language.set_active_from_value(
             self.settings.get("general", "language")
         )
-
+        self.view.get_widget("enable_attributes").set_active(
+            int(self.settings.get("general", "enable_attributes"))
+        )
+        self.view.get_widget("enable_emblems").set_active(
+            int(self.settings.get("general", "enable_emblems"))
+        )
+        self.view.get_widget("enable_recursive").set_active(
+            int(self.settings.get("general", "enable_recursive"))
+        )
+        self.view.get_widget("diff_tool").set_text(
+            self.settings.get("external", "diff_tool")
+        )
+        self.view.get_widget("diff_tool_swap").set_active(
+            int(self.settings.get("external", "diff_tool_swap"))
+        )
+        self.view.get_widget("repo_browser").set_text(
+            self.settings.get("external", "repo_browser")
+        )
+        self.view.get_widget("cache_number_repositories").set_text(
+            self.settings.get("cache", "number_repositories")
+        )
+        self.view.get_widget("cache_number_messages").set_text(
+            self.settings.get("cache", "number_messages")
+        )
+        
     def on_destroy(self, widget):
         gtk.main_quit()
 
