89902a373508f76eb3b6c39cfe5a2db1eb040b00
adamplumb <adamplumb@c1aae091-0650-0410-8f76-b90164cd595d>
2008-12-15 16:27:25 +0000

    added about window

---
+++ branches/v0.12/nautilussvn/ui/about.py
@@ -0,0 +1,48 @@
+#
+# This is an extension to the Nautilus file manager to allow better 
+# integration with the Subversion source control system.
+# 
+# Copyright (C) 2008 NautilusSvn Team
+# 
+# NautilusSvn is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# NautilusSvn is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with NautilusSvn;  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import pygtk
+import gobject
+import gtk
+
+import nautilussvn.ui
+
+class About:
+    """
+    This class provides an interface to the About window.
+    Displays:
+        Authors/Credits
+        Version Information
+        Links
+    
+    """
+
+    def __init__(self):
+        self.view = nautilussvn.ui.InterfaceView(self, "about", "About")
+
+    def on_destroy(self, widget):
+        gtk.main_quit()
+
+    def on_close_clicked(self, widget):
+        gtk.main_quit()
+        
+if __name__ == "__main__":
+    window = About()
+    gtk.main()
