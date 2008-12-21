f0d4f27d5ae235362fd569c0fad7224a681dfcca
adamplumb <adamplumb@c1aae091-0650-0410-8f76-b90164cd595d>
2008-12-21 16:31:00 +0000

    added get_items method to get items with a given status

--- branches/v0.12/nautilussvn/lib/vcs/svn.py
+++ branches/v0.12/nautilussvn/lib/vcs/svn.py
@@ -24,6 +24,31 @@ from os.path import isdir, isfile
 import pysvn
 
 class SVN:
+
+    STATUS_DICT = {
+        pysvn.wc_status_kind.none:          "None",
+        pysvn.wc_status_kind.unversioned:   "Unversioned",
+        pysvn.wc_status_kind.normal:        "Normal",
+        pysvn.wc_status_kind.added:         "Added",
+        pysvn.wc_status_kind.missing:       "Missing",
+        pysvn.wc_status_kind.deleted:       "Deleted",
+        pysvn.wc_status_kind.replaced:      "Replaced",
+        pysvn.wc_status_kind.modified:      "Modified",
+        pysvn.wc_status_kind.merged:        "Merged",
+        pysvn.wc_status_kind.conflicted:    "Conflicted",
+        pysvn.wc_status_kind.ignored:       "Ignored",
+        pysvn.wc_status_kind.obstructed:    "Obstructed",
+        pysvn.wc_status_kind.external:      "External",
+        pysvn.wc_status_kind.incomplete:    "Incomplete"
+    }
+    
+    STATUS_CHANGED_DICT = {
+        pysvn.wc_status_kind.unversioned:   "Unversioned",
+        pysvn.wc_status_kind.added:         "Added",
+        pysvn.wc_status_kind.deleted:       "Deleted",
+        pysvn.wc_status_kind.replaced:      "Replaced",
+        pysvn.wc_status_kind.modified:      "Modified",
+    }
     
     def __init__(self):
         self.client = pysvn.Client()
@@ -132,3 +157,44 @@ class SVN:
                 return True
         
         return False
+        
+        
+    #
+    # provides information for ui
+    #
+    
+    def get_items(self, paths, statuses=None):
+        """
+        Retrieves a list of files that have one of a set of statuses
+        
+        @type   paths: list
+        @param  paths: a list of paths or files
+        
+        @type   statuses: list
+        @param  statuses: a list of pysvn.wc_status_kind statuses
+        
+        @rtype list
+        @returner a list of PysvnStatus objects
+        
+        """
+
+        if paths is None:
+            return []
+        
+        #recursively searches the common path of all "paths" item
+        st = self.client.status(
+            os.path.commonprefix(paths)[len(os.getcwd()):]
+        )
+
+        if st is None:
+            return []
+
+        returner = []
+        for st_item in st:
+            if statuses is not None:
+                if st_item.text_status not in statuses:
+                    continue
+                
+            returner.append(st_item)
+        
+        return returner
