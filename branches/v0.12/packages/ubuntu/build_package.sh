4920b4eb1552e0938de1f8be3e8a1c5d4f5c3dad
brucevdkooij <brucevdkooij@c1aae091-0650-0410-8f76-b90164cd595d>
2008-12-26 22:18:47 +0000

    Corrected E: copyright-should-refer-to-common-license-file-for-gpl

--- branches/v0.12/packages/ubuntu/build_package.sh
+++ branches/v0.12/packages/ubuntu/build_package.sh
@@ -3,8 +3,10 @@
 # FIXME: this is obviously just an example of the procedure, still need to actually
 # write it properly (get the version number from somewhere).
 
-svn export . /tmp/nautilussvn-0.12
-cd /tmp
+rm -rf /tmp/nautilussvn_build
+mkdir /tmp/nautilussvn_build
+svn export . /tmp/nautilussvn_build/nautilussvn-0.12
+cd /tmp/nautilussvn_build
 tar -zcvf nautilussvn_0.12.orig.tar.gz nautilussvn-0.12
 cd nautilussvn-0.12/
 cd packages/ubuntu/
