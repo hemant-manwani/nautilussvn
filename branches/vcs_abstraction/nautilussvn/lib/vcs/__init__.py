15fd728d3815dec15d179366ece0fad2a5f67787
brucevdkooij <brucevdkooij@c1aae091-0650-0410-8f76-b90164cd595d>
2009-01-01 11:27:26 +0000

    With changes now

--- branches/vcs_abstraction/nautilussvn/lib/vcs/__init__.py
+++ branches/vcs_abstraction/nautilussvn/lib/vcs/__init__.py
@@ -20,12 +20,32 @@
 # along with NautilusSvn;  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from svn import SVN
-from ..decorators import deprecated
+from nautilussvn.lib import StatusMonitor
+from nautilussvn.lib.vcs.svn import SVNClient, SVNStatusMonitor
+from nautilussvn.lib.decorators import deprecated
 
-class VCS:
-    pass
+@deprecated
+def create_vcs_instance():
+    # TODO: remove this once all classes are converted to this new style
+    return SVNClient()
     
+class VCSClient:
+    
+    def is_valid_path(self, path):
+        """
+        This function is used to verify whether a certain path is a working
+        copy for a particular VCS.
+        
+        This function needs to be implemented by all subclasses.
+        
+        @type   path:   string
+        @type   path:   The path for which to check what VCS it belongs to.
+        
+        @rtype:         boolean
+        @return:        A boolean indicating whether path belongs to VCS.
+        """
+        
+        return False
 
 class VCSFactory:
     
@@ -36,7 +56,72 @@ class VCSFactory:
         """
         
         return SVN()
+        
+class VCSStatusMonitor():
+    """
+    
+    The C{StatusMonitor} is basically a replacement for the currently limited 
+    C{update_file_info} function.
+    
+    What C{StatusMonitor} does:
+    
+      - When somebody adds a watch and if there's not already a watch for this 
+        item it will add one and do an initial status check.
+    
+      - Use inotify to keep track of modifications of any watched items
+        (we actually only care about modifications not creations and deletions)
+        
+      - Either on request, or when something interesting happens, it checks
+        the status for an item which means:
+        
+          - See working code for exactly what a status check means
+        
+          - After checking the status for an item, if there's a watch for
+            a parent directory this is what will happen:    
+        
+              - If status is (vcs) modified, (vcs) added or (vcs) deleted:
+            
+                  - For every parent the callback will be called with status 
+                    "modified" (since it cannot be any other way)
+          
+              - If vcs status is normal: 
+            
+                  - A status check is done for the parent directory since we 
+                    cannot be sure what the status for them is
+      
+    In the future we might implement a functionality which also monitors
+    versioning actions so the command-line client can be used and still have
+    the emblems update accordingly. 
+    
+    UML sequence diagram depicting how the StatusMonitor is used::
 
-def create_vcs_instance():
-    # TODO: we'll figure this out later by looking at the working copy.
-    return SVN()
+        +---------------+          +-----------------+         
+        |  NautilusSVN  |          |  StatusMonitor  |         
+        +---------------+          +-----------------+         
+               |                            |                  
+               |   new(self.cb_status)      |                  
+               |--------------------------->|                  
+               |                            |                  
+               |     add_watch(path)        |                  
+               |--------------------------->|---+              
+               |                            |   |              
+               |  cb_status(path, status)   |   | status(path) 
+               |<---------------------------|<--+              
+               |                            |                  
+               |---+                        |                  
+               |   | set_emblem_by_status(path, status)      
+               |<--+                        |                  
+               |                            |                  
+
+    
+    """
+    
+    def __init__(self, callback):
+        self.status_monitor = StatusMonitor(self.callback)
+        self.vcs_status_monitor = SVNStatusMonitor(callback, self.status_monitor)
+        
+    def add_watch(self, path):
+        self.vcs_status_monitor.add_watch(path)
+        
+    def callback(self, path):
+        self.vcs_status_monitor.status(path, invalidate=True)
