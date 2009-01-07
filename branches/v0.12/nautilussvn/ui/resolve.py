fc2851a6682f7e59f6c3239ee20fcfce33cfa9af
adamplumb <adamplumb@c1aae091-0650-0410-8f76-b90164cd595d>
2009-01-07 14:48:58 +0000

    Implemented the resolve ui

--- branches/v0.12/nautilussvn/ui/resolve.py
+++ branches/v0.12/nautilussvn/ui/resolve.py
@@ -24,15 +24,68 @@ import pygtk
 import gobject
 import gtk
 
+from nautilussvn.ui import InterfaceView
 from nautilussvn.ui.add import Add
+from nautilussvn.ui.callback import VCSAction
+import nautilussvn.ui.widget
+import nautilussvn.ui.dialog
+import nautilussvn.ui.callback
+import nautilussvn.lib.helper
 
 class Resolve(Add):
     def __init__(self, paths):
-        Add.__init__(self, paths)
-        
+        InterfaceView.__init__(self, "add", "Add")
+
         self.window = self.get_widget("Add")
         self.window.set_title("Resolve")
 
+        self.last_row_clicked = None
+
+        self.files_table = nautilussvn.ui.widget.Table(
+            self.get_widget("files_table"), 
+            [gobject.TYPE_BOOLEAN, gobject.TYPE_STRING, gobject.TYPE_STRING], 
+            [nautilussvn.ui.widget.TOGGLE_BUTTON, "Path", "Extension"]
+        )
+
+        self.vcs = nautilussvn.lib.vcs.create_vcs_instance()
+        self.files = self.vcs.get_items(
+            paths, 
+            [self.vcs.STATUS["conflicted"]]
+        )
+        
+        for item in self.files:
+            self.files_table.append([
+                True, 
+                item.path, 
+                nautilussvn.lib.helper.get_file_extension(item.path)
+            ])
+                    
+    def on_ok_clicked(self, widget):
+        items = self.files_table.get_activated_rows(1)
+        self.hide()
+
+        self.action = nautilussvn.ui.callback.VCSAction(
+            self.vcs,
+            register_gtk_quit=self.gtk_quit_is_set()
+        )
+        
+        self.action.append(self.action.set_status, "Running Resolve Command...")
+        for item in items:
+            self.action.append(self.vcs.resolve, item, recurse=True)
+        self.action.append(self.action.set_status, "Completed Resolve")
+        self.action.append(self.action.finish)
+        self.action.start()
+
+    #
+    # Context Menu Conditions
+    #
+    
+    def condition_delete(self):
+        return False
+
+    def condition_ignore_submenu(self):
+        return False
+        
 if __name__ == "__main__":
     import sys
     args = sys.argv[1:]
