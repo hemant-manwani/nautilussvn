9801d907f8b40532365c0c3ca7c8ef8a13f6f769
brucevdkooij <brucevdkooij@c1aae091-0650-0410-8f76-b90164cd595d>
2009-02-21 17:17:38 +0000

    Looks like this doesn't do anything asynchronously, so removing.

--- branches/v0.12/nautilussvn/lib/dbus/__init__.py
+++ branches/v0.12/nautilussvn/lib/dbus/__init__.py
@@ -19,6 +19,3 @@
 # You should have received a copy of the GNU General Public License
 # along with NautilusSvn;  If not, see <http://www.gnu.org/licenses/>.
 #
-
-def generic_reply_handler(*args): pass
-def generic_error_handler(e): raise e
