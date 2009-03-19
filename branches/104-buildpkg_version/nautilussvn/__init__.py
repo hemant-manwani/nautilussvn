7251bc5b67a55c3edbd76924f7514de049a65023
allister.macleod <allister.macleod@c1aae091-0650-0410-8f76-b90164cd595d>
2009-03-19 17:06:55 +0000

    - Allister MacLeod added the remaining package_* methods.

--- branches/104-buildpkg_version/nautilussvn/__init__.py
+++ branches/104-buildpkg_version/nautilussvn/__init__.py
@@ -51,3 +51,22 @@ def package_name():
 
     """
     return APP_NAME.lower()
+
+
+def package_version():
+    """
+    Report the version number of the application, minus any name
+    extensions.
+
+    """
+    app_version = version.split('-')[0]
+    # TODO: sanity-check app_version: make sure it's just digits and dots
+    return app_version
+
+
+def package_identifier():
+    """
+    Return a package identifier suitable for use in a package file.
+
+    """
+    return "%s-%s" % (package_name(), package_version())
