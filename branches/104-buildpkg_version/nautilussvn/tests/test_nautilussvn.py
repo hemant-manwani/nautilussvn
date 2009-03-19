5671423f65ea999159c7a86035f122c11dae7553
allister.macleod <allister.macleod@c1aae091-0650-0410-8f76-b90164cd595d>
2009-03-19 16:57:47 +0000

    - Allister MacLeod added a broken package_name() method to the nautilussvn package, and wrote a test to show how broken it is.  Time to fix it!

--- branches/104-buildpkg_version/nautilussvn/tests/test_nautilussvn.py
+++ branches/104-buildpkg_version/nautilussvn/tests/test_nautilussvn.py
@@ -1,52 +1,44 @@
 #
-# This is an extension to the Nautilus file manager to allow better 
+# This is an extension to the Nautilus file manager to allow better
 # integration with the Subversion source control system.
-# 
+#
 # Copyright (C) 2006-2008 by Jason Field <jason@jasonfield.com>
 # Copyright (C) 2007-2008 by Bruce van der Kooij <brucevdkooij@gmail.com>
 # Copyright (C) 2008-2008 by Adam Plumb <adamplumb@gmail.com>
-# 
+#
 # NautilusSvn is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # NautilusSvn is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with NautilusSvn;  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import os
-import gettext as _gettext
-from locale import getdefaultlocale
+"""
+Unit tests for the top-level nautilussvn package.
 
-version = "0.12-beta1"
-APP_NAME = "NautilusSvn"
-LOCALE_DIR = "%s/locale" % os.path.dirname(os.path.abspath(__file__))
-if not os.path.exists(LOCALE_DIR):
-    LOCALE_DIR = "/usr/share/locale"
+"""
 
-langs = []
-language = os.environ.get('LANGUAGE', None)
-if language:
-    langs += language.split(":")
-langs += [getdefaultlocale()[0]]
-if not langs:
-    langs = ["en_US"]
+from unittest import TestCase, main
+import nautilussvn
 
-_gettext.bindtextdomain(APP_NAME, LOCALE_DIR)
-_gettext.textdomain(APP_NAME)
 
-gettext = _gettext.translation(APP_NAME, LOCALE_DIR, languages=langs, fallback=True)
-
-def package_name():
+class NautilusSvnTest(TestCase):
     """
-    Report the application name in a form appropriate for building
-    package files.
+    Main NautilusSvn tests.
 
     """
-    return lower(APP_NAME)
+    def test_package_name(self):
+        """Make sure the package name is reported properly."""
+        result = nautilussvn.package_name()
+        self.assertEqual(result, "nautilussvn")
+
+
+if __name__ == "__main__":
+    main()
