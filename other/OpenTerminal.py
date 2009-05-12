19328188af092b9c0379887f77574fb0ce308b2b
brucevdkooij <brucevdkooij@c1aae091-0650-0410-8f76-b90164cd595d>
2009-05-12 09:40:33 +0000

    I use this instead of nautilus-openterminal

---
+++ other/OpenTerminal.py
@@ -0,0 +1,50 @@
+import os.path
+from os.path import isdir
+import re
+import urllib
+from subprocess import Popen
+
+import nautilus
+
+class OpenTerminalExtension(nautilus.MenuProvider):
+
+    def __init__(self):
+        pass
+
+    def get_file_items(self, window, items):
+        if len(items) == 0: return
+        item = items[0]
+
+        menu_item = nautilus.MenuItem(
+            "OpenTerminal::OpenTerminal",
+            "Open In Terminal",
+            "",
+            "gnome-terminal"
+        )
+
+        def open_terminal(menu_item):
+            """
+            Ignore the ugliness, quick hack.
+            """
+            uri = urllib.unquote(item.get_uri())
+
+            if item.get_uri().startswith("x-nautilus-desktop://"):
+                Popen(["gnome-terminal", "--working-directory", "~/Desktop"]).pid
+            elif item.get_uri().startswith("file://"):
+                path = uri.replace("file://", "")
+                if not isdir(path): path = os.path.dirname(path)
+                Popen(["gnome-terminal", "--working-directory", path]).pid
+            elif item.get_uri().startswith("sftp://"):
+                hostname, path = re.match("sftp://([^/]+)(/.*)", uri).groups()
+                dirname = os.path.dirname(path) # might have selected a file
+                ssh_command = 'ssh %s -t \'cd \\"%s\\" || cd \\"%s\\"; $SHELL -l\'' % (hostname, path, dirname)
+                Popen(["gnome-terminal", "-e", ssh_command]).pid
+
+        menu_item.connect("activate", open_terminal)
+
+        return [menu_item]
+
+
+    def get_background_items(self, window, item):
+        return self.get_file_items(window, [item])
+
