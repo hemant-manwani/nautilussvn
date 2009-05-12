89c7779aea961baff24d9ee101a7e392390bb325
brucevdkooij <brucevdkooij@c1aae091-0650-0410-8f76-b90164cd595d>
2009-05-12 01:20:30 +0000

    Just thought up of this while lying in bed. This improves performance when changes occur, by doing the parent_dir check in add_to_queue you eliminate a lot of the same checks. This time it actually ignores all temporary files properly because we just ignore everything that is unversioned (we don't care about them).

--- branches/status_monitor_reincarnation/nautilussvn/statusmonitor.py
+++ branches/status_monitor_reincarnation/nautilussvn/statusmonitor.py
@@ -236,19 +236,41 @@ class StatusMonitor:
         if path.find(".svn") != -1 and not path.endswith(".svn/entries"): return
         if path.find(".git") != -1: return # FIXME: not handling Git right now
         
-        print os.path.basename(path), event_type
+        print path, event_type
         
         # TODO: if we can actually figure out specifically what file was
         # changed by looking at the entries file this would be a lot easier
+        parent_dir = None
         if path.endswith(".svn/entries"):
-            working_dir = os.path.abspath(os.path.join(os.path.dirname(path), ".."))
+            parent_dir = working_dir = os.path.abspath(os.path.join(os.path.dirname(path), ".."))
             paths = [os.path.join(working_dir, basename) for basename in os.listdir(working_dir)]
             for path in paths:
-                # FIXME: this ignores propchanges for the moment
+                # FIXME: this ignores propchanges on the directory for the moment
                 if not isdir(path): 
                     self.status(path, invalidate=True, recursive=False)
         else:
-            self.status(path, invalidate=True, recursive=False)
+            # Let's try and do some fancy stuff to detect if we were 
+            # dealing with an unversioned file, this is a little bit
+            # hard because like mentioned before, Subversion is one of
+            # the few VCS's where directories actually matter.
+            workdir_manager = get_workdir_manager_for_path(path)
+            statuses_dictionary = dict([
+                (status.abspath, status.state) 
+                for status in workdir_manager.status(paths=(path,), recursive=False)
+                if status.state != "unknown"
+            ])
+            
+            if isdir(path) or path in statuses_dictionary:
+                self.status(path, invalidate=True, recursive=False)
+                parent_dir = os.path.split(path)[0]
+        
+        # Refresh the status for all parents
+        if parent_dir:
+            path_to_check = parent_dir
+            while path_to_check != "/":
+                if not get_workdir_manager_for_path(path_to_check): break
+                self.status(path_to_check, recursive=False)
+                path_to_check = os.path.split(path_to_check)[0]
     
     def add_to_queue(self, path, invalidate, recursive):
         if (path, invalidate, recursive) in self.status_queue: return
@@ -294,17 +316,6 @@ class StatusMonitor:
                             statuses = [(status.abspath, status.state) for status in 
                                 workdir_manager.status(paths=(path,), recursive=recursive)]
                             self.update_status_tree(statuses)
-                            
-                            # Refresh the status for all parents
-                            # FIXME: this isn't very ideal yet, if the 
-                            # parent directories haven't yet been checked
-                            # this will generate a massive amount of status
-                            # checks
-                            path_to_check = path
-                            while path_to_check != "/":
-                                path_to_check = os.path.split(path_to_check)[0]
-                                if not get_workdir_manager_for_path(path_to_check): break
-                                self.status(path_to_check, recursive=False)
                         except OSError:
                             pass
                     
