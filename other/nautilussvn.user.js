26de9aadd00e79c25137d602ecc9027282fbb146
brucevdkooij <brucevdkooij@c1aae091-0650-0410-8f76-b90164cd595d>
2009-05-24 10:22:50 +0000

    Cleaned up and added saved searches to Greasemonkey script

--- other/nautilussvn.user.js
+++ other/nautilussvn.user.js
@@ -5,9 +5,29 @@
 // @include        http://code.google.com/p/nautilussvn/*
 // ==/UserScript==
 
+var base_url = "http://code.google.com/p/nautilussvn/issues/list"
+var custom_issues_query = "-status:Stalled+-milestone:Future+-priority:low+-owner:adamplumb"
+var saved_searches = {
+    "All": "",
+    "New bugs": "type:Defect+status:New",
+    "Bugs": "type:Defect",
+    "Enhancements":  "type:Enhancement"
+}
 
+// Change the default tab to point to a customized query
 var issue_links = document.evaluate("//a[@href='/p/nautilussvn/wiki/Issues?tm=3']", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
 for (var i = 0 ; i < issue_links.snapshotLength; i++) {
   var issue_link = issue_links.snapshotItem(i)
-  issue_link.href = "http://code.google.com/p/nautilussvn/issues/list?can=2&q=-status%3AStalled+-milestone%3AFuture+-priority%3Alow+-owner%3Aadamplumb&colspec=Stars+ID+Type+Status+Priority+Milestone+Owner+Summary&x=stars&y=status&cells=tiles"
+  issue_link.href = base_url + "?q=" + custom_issues_query
 }
+
+var main_column = document.evaluate("id('maincol')", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+var saved_searches_container = document.createElement("div")
+saved_searches_container.setAttribute("style", "margin-bottom: 0.5em;")
+for (var key in saved_searches) {
+    saved_searches_container.innerHTML += '<a href="' + base_url + "?q=" + saved_searches[key] + '">' + key + '</a> '
+}
+
+main_column.insertBefore(
+    saved_searches_container, main_column.firstChild
+)
