e8a11b1df2b01619d775a058370b7d9dea7f9142
brucevdkooij <brucevdkooij@c1aae091-0650-0410-8f76-b90164cd595d>
2009-06-27 20:42:06 +0000

    This isn't done yet but committing because I'm feeling tired

---
+++ branches/status_monitor_threaded/nautilussvn/extension/nautilus/NautilusSvn.py
@@ -0,0 +1,165 @@
+"""
+
+Our module for everything related to the Nautilus extension.
+  
+"""
+
+from __future__ import with_statement
+
+from os.path import realpath
+
+import nautilus
+import gnomevfs
+
+from nautilussvn.daemon.statuschecker import StatusChecker
+from nautilussvn.util.vcs import get_summarized_status
+
+class NautilusSvn(nautilus.InfoProvider, nautilus.MenuProvider):
+
+    #: Maps statuses to emblems.
+    #: TODO: should probably be possible to create this dynamically
+    EMBLEMS = {
+        "added" :       "nautilussvn-added",
+        "deleted":      "nautilussvn-deleted",
+        "removed":      "nautilussvn-deleted",
+        "modified":     "nautilussvn-modified",
+        "conflicted":   "nautilussvn-conflicted",
+        "missing":      "nautilussvn-conflicted",
+        "normal":       "nautilussvn-normal",
+        "clean":        "nautilussvn-normal",
+        "ignored":      "nautilussvn-ignored",
+        "locked":       "nautilussvn-locked",
+        "read_only":    "nautilussvn-read_only",
+        "obstructed":   "nautilussvn-obstructed",
+        "unknown":      "nautilussvn-unknown",
+        "unversioned":  "nautilussvn-unknown"
+    }
+    
+    #: This is our lookup table for C{NautilusVFSFile}s which we need for attaching
+    #: emblems. This is mostly a workaround for not being able to turn a path/uri
+    #: into a C{NautilusVFSFile}. It looks like:::
+    #: 
+    #:     nautilusVFSFile_table = {
+    #:        "/foo/bar/baz": <NautilusVFSFile>
+    #:     
+    #:     }
+    #: 
+    #: Keeping track of C{NautilusVFSFile}s is a little bit complicated because
+    #: when an item is moved (renamed) C{update_file_info} doesn't get called. So
+    #: we also add C{NautilusVFSFile}s to this table from C{get_file_items} which
+    #: gets called when an item is selected.
+    nautilusVFSFile_table = {}
+    
+    def __init__(self):
+        # Create our StatusChecker to requests statuses
+        self.status_checker = StatusChecker()
+        self.status_checker.start()
+        
+    def update_file_info(self, item):
+        """
+        C{update_file_info} is called when:
+        
+          - When you enter a directory (once for each item but only when the
+            item was modified since the last time it was listed)
+          - When you refresh (once for each item visible)
+          - When an item viewable from the current window is created or modified
+        
+        You are not notified about items that are not visible.
+        """
+        
+        if not self.valid_uri(item.get_uri()): return
+        path = realpath(gnomevfs.get_local_path_from_uri(item.get_uri()))
+        
+        # Always replace the item in the table with the one we receive, because
+        # for example if an item is deleted and recreated the NautilusVFSFile
+        # we had before will be invalid (think pointers and such).
+        self.nautilusVFSFile_table[path] = item
+        
+        # TODO: 
+        #   1. Request the statuses for the item from the daemon
+        #   2. Calculate a summarized status
+        #   3. Add an emblem
+        # (we could possibly skip the first two if we kept a cache too)
+        
+        self.status_checker.check_status(path, callback=self.cb_status)
+        
+        
+    def get_file_items(self, window, items):
+        """
+        Menu activated with items selected. Nautilus also calls this function
+        when rendering submenus, even though this is not needed since the entire
+        menu has already been returned.
+        
+        Note that calling C{nautilusVFSFile.invalidate_extension_info()} will 
+        also cause this function to be called.
+        
+        @type   window: NautilusNavigationWindow
+        @param  window:
+        
+        @type   items:  list of NautilusVFSFile
+        @param  items:
+        
+        @rtype:         list of MenuItems
+        @return:        The context menu entries to add to the menu.
+        
+        """
+        
+        paths = []
+        for item in items:
+            if self.valid_uri(item.get_uri()):
+                path = realpath(gnomevfs.get_local_path_from_uri(item.get_uri()))
+                paths.append(path)
+                self.nautilusVFSFile_table[path] = item
+
+        if len(paths) == 0: return []
+        
+        # TODO:
+        return []
+        
+    def get_background_items(self, window, item):
+        """
+        Menu activated on entering a directory. Builds context menu for File
+        menu and for window background.
+        
+        @type   window: NautilusNavigationWindow
+        @param  window:
+        
+        @type   item:   NautilusVFSFile
+        @param  item:
+        
+        @rtype:         list of MenuItems
+        @return:        The context menu entries to add to the menu.
+        
+        """
+        
+        if not self.valid_uri(item.get_uri()): return
+        path = realpath(gnomevfs.get_local_path_from_uri(item.get_uri()))
+        self.nautilusVFSFile_table[path] = item
+        
+        # TODO:
+        return []
+    
+    #=== HELPER FUNCTIONS ======================================================
+    
+    def valid_uri(self, uri):
+        """
+        Check whether or not it's a good idea to have NautilusSvn do
+        its magic for this URI. Some examples of URI schemes:
+        
+        x-nautilus-desktop:/// # e.g. mounted devices on the desktop
+        
+        """
+        
+        # TODO: integrate this into the settings manager and allow people
+        # to add custom rules etc.
+        
+        if not uri.startswith("file://"): return False
+        
+        return True
+        
+    #=== CALLBACKS =============================================================
+    
+    def cb_status(self, path, statuses):
+        if not path in self.nautilusVFSFile_table: return
+        item = self.nautilusVFSFile_table[path]
+        item.add_emblem(self.EMBLEMS[get_summarized_status(path, statuses)])
