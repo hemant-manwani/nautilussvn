e1557af5afb5fb633b13b8aae0c4eced0085b171
brucevdkooij <brucevdkooij@c1aae091-0650-0410-8f76-b90164cd595d>
2009-09-27 14:41:26 +0000

    Making some changes in absolute URLs

--- website/dokuwiki/rabbitvcs/footer.php
+++ website/dokuwiki/rabbitvcs/footer.php
@@ -3,7 +3,7 @@
             <div id="page-footer" role="contentinfo">
                 <div id="page-footer-container">
                     <p class="copyright">
-                        All text and image content on rabbitvcs.org, unless otherwise specified, is licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-Share Alike 3.0 License</a>. This does not include the RabbitVCS name, logo or icon. <a href="http://wiki.rabbitvcs.org/copyright">More...</a>
+                        All text and image content on rabbitvcs.org, unless otherwise specified, is licensed under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-Share Alike 3.0 License</a>. This does not include the RabbitVCS name, logo or icon. <a href="http://wiki.rabbitvcs.org/wiki/copyright">More...</a>
                     </p>
                     <a href="http://creativecommons.org/licenses/by-sa/3.0/"><img src="http://i.creativecommons.org/l/by-sa/3.0/us/88x31.png"/></a>
                 </div>
